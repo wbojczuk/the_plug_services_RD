ba8a641e9f0c7a63fee2a17cffc0e746f1e5b846
William Bojczuk <williambojczuk@gmail.com>
2024-03-05 15:01:39 +0200

    updated

--- src/app/(pages)/reviews/page.tsx
+++ src/app/(pages)/reviews/page.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next"
-import ReviewHeader from "../../../../fordev/Reviews/Reviews Style 1/ReviewHeader/ReviewHeader"
-import Reviews from "../../../../fordev/Reviews/Reviews Style 1/Reviews/Reviews"
+
 
 export const metadata: Metadata = {
     title: "Our Reviews",
@@ -10,8 +9,7 @@ export const metadata: Metadata = {
 export default function ReviewPage(){
     return(
         <>
-            <ReviewHeader />
-            <Reviews />
+            
         </>
     )
 }
