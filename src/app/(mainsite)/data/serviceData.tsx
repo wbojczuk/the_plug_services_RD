17ab388e89867cf616f7b8ab76d2e30a130605fb
William Bojczuk <williambojczuk@gmail.com>
2024-03-10 18:35:02 +0200

    updated

--- src/app/(mainsite)/data/serviceData.tsx
+++ src/app/(mainsite)/data/serviceData.tsx
@@ -11,7 +11,7 @@ const serviceData: serviceType[] = [
     },
     {
         title: "Software Development & IT",
-        description: "Will play a crucial role in developing and maintaining our software applications, systems, and IT infrastructure. They will work closely with other team members to innovate and create efficient solutions that meet your business needs",
+        description: "Will play a crucial role in developing and maintaining your software applications, systems, and IT infrastructure. They will work closely with other team members to innovate and create efficient solutions that meet your business needs",
         icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path fill="currentColor" d="M8 4v4H4V4zM2 2v8h8V2zm16 5v4h-4V7zm-6-2v8h8V5zM8 16v4H4v-4zm-6-2v8h8v-8z"></path><path fill="currentColor" d="M22 10v6h-6v6h-6v8h20V10Zm-4 8h4v4h-4Zm-2 10h-4v-4h4Zm6 0h-4v-4h4Zm6 0h-4v-4h4Zm0-6h-4v-4h4Zm-4-6v-4h4v4Z"></path></svg>
     },
     {
